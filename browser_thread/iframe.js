0edc89d9c15c4412d4dda4f4d2fe22f4b571dccb
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-03-27 10:14:49 -0500

    style: js-beautify formatting

--- browser_thread/iframe.js
+++ browser_thread/iframe.js
@@ -1,7 +1,7 @@
 const viewX = [35.4, 283.6],
-	invader = document.getElementById("invader");
+  invader = document.getElementById("invader");
 let frame = 0;
 setInterval(() => {
-	frame = ++frame % 2;
-	invader.viewBox.baseVal.x = viewX[frame];
+  frame = ++frame % 2;
+  invader.viewBox.baseVal.x = viewX[frame];
 }, 200);
--- browser_thread/index.js
+++ browser_thread/index.js
@@ -1,11 +1,11 @@
 setInterval(() => {
-	for (let i = 0; i < 2090000000; i++) {}
+  for (let i = 0; i < 2090000000; i++) {}
 }, 1000);
 
 const viewX = [35.4, 283.6],
-	invader = document.getElementById("invader");
+  invader = document.getElementById("invader");
 let frame = 0;
 setInterval(() => {
-	frame = ++frame % 2;
-	invader.viewBox.baseVal.x = viewX[frame];
+  frame = ++frame % 2;
+  invader.viewBox.baseVal.x = viewX[frame];
 }, 0);
